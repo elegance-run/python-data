33536169a90db2ced387e2acd7982e9fe0755c4b
zhengrun <zhengrun1101@163.com>
2016-11-06 11:02:44 +0800

    sina_get.py

--- sina_get.py
+++ sina_get.py
@@ -4,6 +4,6 @@ url='http://www.sina.com.cn'
 
 sina_content=get_content(url)
 
-print("____________________________")
-print("sina_get得到内容前500个字符：",sina_content[:500])
+print("---------------------------------------")
+print("sina_get得到内容前60个字符：",sina_content[:60])
 print("sina_get得到的内容长度：",len(sina_content))
