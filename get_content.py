611e2344e7d960540db146b51ea6b146635bd601
zhengrun <zhengrun1101@163.com>
2016-11-06 11:03:25 +0800

    get_content.py

--- get_content.py
+++ get_content.py
@@ -4,16 +4,17 @@ def get_content(url):
     resp=requests.get(url)
     return resp.text
 
-url="http://www.phei.com.cn"
+if __name__=='__main__':
+    url = "http://www.phei.com.cn"
 
-content=get_content(url)
+    content = get_content(url)
 
-print('前50个字符：',content[:500])
+    print('前50个字符：', content[:50])
 
-content_len=len(content)
-print('内容的长度为：',content_len)
+    content_len = len(content)
+    print('内容的长度为：', content_len)
 
-if content_len >= 40*1024:
-    print('内容长度大于等于40KB；')
-else:
-    print('内容长度小于等于40KB；')
+    if content_len >= 40 * 1024:
+        print('内容长度大于等于40KB；')
+    else:
+        print('内容长度小于等于40KB；')
